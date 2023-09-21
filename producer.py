f3e90173ce33b9839a90879d461e0078643f21c1
milad <yousefimiladdd@gmail.com>
2023-09-21 14:55:38 +0330

    handle gender in admin panel

--- producer.py
+++ producer.py
@@ -3,33 +3,39 @@ import pika
 import json
 
 def main():
-    credentials = pika.PlainCredentials('rgbackend', 'hTw6@1l8^Z')
-    parameters = pika.ConnectionParameters(host='80.191.200.176', port=5672, virtual_host='/', credentials=credentials)
-    
+    credentials = pika.PlainCredentials('rg', 'rg123123')
+    parameters = pika.ConnectionParameters(host='192.168.50.35', port=5672, virtual_host='/', credentials=credentials)
+   
     connection = pika.BlockingConnection(parameters)
-    channel = connection.channel()
-    
-    # Declare a queue where you want to send messages
+    channel = connection.channel()    
     channel.queue_declare(queue='#')
-    
-    try:
-        # Create a dictionary in the specified format
-        data = {
-            "data": "0e8d05e3",
-            # "data": "CA3EFEA0",
-            # "data": "F77F44B3",
-            "time": 48587147,
-            "action": "read"
-        }
-        
-        # Convert the dictionary to a JSON string
-        message = json.dumps(data)
-        
-        # Send the JSON message to the queue
-        channel.basic_publish(exchange='', routing_key='pool', body=message)
-        print(f" [x] Sent {message}")
-    except Exception as e:
-        print(f"Error: {e}")
+    uuids=[
+        '0e8d05e3',
+        # 'b7e7efc6',
+        # '05139FB1',
+        # '6A122664',
+        # '77a7c4c6',
+        # 'c546e2b2',
+        # '1250880',
+        # '0748d3c6',
+        # '15df3ab2',
+        # 'CA3EFEA0',
+        # 'F77F44B3',
+        # '2CAC228F',
+        # 'DAAC2779'
+    ]
+    for i in uuids:
+        try:
+            data = {
+                "data": i,
+                "time": 48587147,
+                "action": "read"
+            }
+            message = json.dumps(data)
+            channel.basic_publish(exchange='', routing_key='shahin', body=message)
+            print(f" [x] Sent {message}")
+        except Exception as e:
+            print(f"Error: {e}")
     
     connection.close()
 
